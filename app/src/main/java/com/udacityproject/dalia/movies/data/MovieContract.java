5a2a7f1bc616b6d9a585509ef85a1e046dcb978c
DaliaAyman <dalia.ay.ahmed@gmail.com>
2015-09-15 03:48:45 +0200

    Init stage 2

---
+++ app/src/main/java/com/udacityproject/dalia/movies/data/MovieContract.java
@@ -0,0 +1,14 @@
+package com.udacityproject.dalia.movies.data;
+
+import android.net.Uri;
+
+/**
+ * Created by Dalia on 9/15/2015.
+ */
+public class MovieContract {
+
+    public static final String CONTENT_AUTHORITY = "com.udacityproject.dalia.movies";
+    public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
+
+    
+}
