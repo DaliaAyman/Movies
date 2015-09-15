e912e2140a3757db0d49a39dbab9783f2d825d05
DaliaAyman <dalia.ay.ahmed@gmail.com>
2015-09-15 03:59:52 +0200

    MovieContract added

--- app/src/main/java/com/udacityproject/dalia/movies/data/MovieContract.java
+++ app/src/main/java/com/udacityproject/dalia/movies/data/MovieContract.java
@@ -1,6 +1,9 @@
 package com.udacityproject.dalia.movies.data;
 
+import android.content.ContentResolver;
+import android.content.ContentUris;
 import android.net.Uri;
+import android.provider.BaseColumns;
 
 /**
  * Created by Dalia on 9/15/2015.
@@ -10,5 +13,29 @@ public class MovieContract {
     public static final String CONTENT_AUTHORITY = "com.udacityproject.dalia.movies";
     public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
 
+    public static final String PATH_MOVIE = "movie";
 
+    /* Inner class that defines the table contents of the movie table */
+    public static class MovieEntry implements BaseColumns{
+
+        public static final Uri CONTENT_URI = BASE_CONTENT_URI.buildUpon().appendPath(PATH_MOVIE).build();
+
+        public static final String CONTENT_TYPE =
+                ContentResolver.CURSOR_DIR_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_MOVIE;
+        public static final String CONTENT_ITEM_TYPE =
+                ContentResolver.CURSOR_ITEM_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_MOVIE;
+
+        public static final String TABLE_NAME = "movie";
+
+        //Columns
+        public static final String COLUMN_MOVIE_TITLE = "title";
+        public static final String COLUMN_MOVIE_POSTER_PATH = "poster_path";
+        public static final String COLUMN_MOVIE_OVERVIEW = "overview";
+        public static final String COLUMN_MOVIE_VOTE_AVERAGE = "vote_average";
+        public static final String COLUMN_MOVIE_RELEASE_DATE = "release_date";
+
+        public static Uri buildMovieUri(long id){
+            return ContentUris.withAppendedId(CONTENT_URI, id);
+        }
+    }
 }
