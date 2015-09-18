303be571cb5bd5bf20b9d6a59bc5114b118e70fd
DaliaAyman <dalia.ay.ahmed@gmail.com>
2015-09-19 01:58:36 +0200

    add uri matcher for movie with ID in MovieProvider

--- app/src/main/java/com/udacityproject/dalia/movies/data/MovieProvider.java
+++ app/src/main/java/com/udacityproject/dalia/movies/data/MovieProvider.java
@@ -14,7 +14,10 @@ public class MovieProvider extends ContentProvider {
     private static final UriMatcher sUriMatcher = buildUriMatcher();
     private MovieDbHelper mOpenHelper;
 
-    static final int MOVIE = 100;
+    static final int MOVIE_BY_MOST_POPULAR = 100;
+    static final int MOVIE_BY_MOST_POPULAR_WITH_ID = 101;
+    static final int MOVIE_BY_HIGHEST_RATED = 200;
+    static final int MOVIE_BY_HIGHEST_RATED_WITH_ID = 201;
 
     @Override
     public boolean onCreate() {
@@ -26,7 +29,10 @@ public class MovieProvider extends ContentProvider {
         final UriMatcher matcher = new UriMatcher(UriMatcher.NO_MATCH);
         final String authority = MovieContract.CONTENT_AUTHORITY;
 
-        matcher.addURI(authority, MovieContract.PATH_MOVIE_MOST_POPULAR, MOVIE);
+        matcher.addURI(authority, MovieContract.PATH_MOVIE_MOST_POPULAR, MOVIE_BY_MOST_POPULAR);
+        matcher.addURI(authority, MovieContract.PATH_MOVIE_MOST_POPULAR + "/#", MOVIE_BY_MOST_POPULAR_WITH_ID);
+        matcher.addURI(authority, MovieContract.PATH_MOVIE_HIGHEST_RATED, MOVIE_BY_HIGHEST_RATED);
+        matcher.addURI(authority, MovieContract.PATH_MOVIE_HIGHEST_RATED + "/#", MOVIE_BY_HIGHEST_RATED_WITH_ID);
 
         return matcher;
     }
@@ -35,8 +41,14 @@ public class MovieProvider extends ContentProvider {
     public String getType(Uri uri) {
         final int match = sUriMatcher.match(uri);
         switch (match){
-            case MOVIE:
-                return MovieContract.MovieEntry.CONTENT_ITEM_TYPE;
+            case MOVIE_BY_MOST_POPULAR:
+                return MovieContract.MovieEntryByMostPopular.CONTENT_TYPE;
+            case MOVIE_BY_MOST_POPULAR_WITH_ID:
+                return MovieContract.MovieEntryByMostPopular.CONTENT_ITEM_TYPE;
+            case MOVIE_BY_HIGHEST_RATED:
+                return MovieContract.MovieEntryByHighestRated.CONTENT_TYPE;
+            case MOVIE_BY_HIGHEST_RATED_WITH_ID:
+                return MovieContract.MovieEntryByHighestRated.CONTENT_ITEM_TYPE;
             default:
                 throw new UnsupportedOperationException("Unknown Uri: " + uri);
         }
